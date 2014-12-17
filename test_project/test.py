2f8df494cb25e9d59943315a6299fcb5eaaeabe9
Sergey Efimov <sargikk@mail.ru>
2014-12-17 13:43:22 +0400

    test count rows in table

--- test_project/test.py
+++ test_project/test.py
@@ -1,3 +1,9 @@
+from django.test import TestCase
 from sample.models import StaffAuthor
 
-print StaffAuthor.sa.query().all()
+
+class AldjemyTestCase(TestCase):
+
+    def test_sa_created(self):
+        StaffAuthor.objects.create(name=u'abacaba', role=u'none', biography=u"test author")
+        assert StaffAuthor.sa.query().count() == 1
